43184ed076e54ee3ca31962c57ddccd8e887d4e8
Anish Ramasekar <anish.ramasekar@gmail.com>
2021-03-29 22:51:11 -0700

    feat: add csi fs client code
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

---
+++ csi/go.mod
@@ -0,0 +1,10 @@
+module github.com/aramase/kubecon-eu-2021/csi
+
+go 1.16
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7
+	k8s.io/klog v1.0.0
+)
---
+++ csi/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/fsnotify.v1"
+	"k8s.io/klog"
+)
+
+var (
+	secretsPath = flag.String("secrets-path", "/mnt/secrets-store", "fs path that contains the secrets")
+	secretName  = flag.String("secret-name", "", "secret name")
+
+	watcher *fsnotify.Watcher
+)
+
+func main() {
+	flag.Parse()
+
+	if len(*secretName) == 0 {
+		klog.Fatal("secret name not provided")
+	}
+
+	secretFile := filepath.Join(*secretsPath, *secretName)
+	secret, err := getSecret(secretFile)
+	if err != nil {
+		klog.Fatalf("failed to get secret file: %v", err)
+	}
+	// log the secret
+	klog.Infof(secret)
+
+	// To receive automatic updates and refresh when the secret in file system is rotated
+	// we will setup a file watcher using fsnotify
+	// creates a new file watcher
+	watcher, _ = fsnotify.NewWatcher()
+	defer watcher.Close()
+
+	// add the secrets dir to the watcher
+	if err = watcher.Add(*secretsPath); err != nil {
+		klog.Fatalf("failed to add secret path to file watcher: %v", err)
+	}
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			// watch for events
+			case <-watcher.Events:
+				secret, err := getSecret(secretFile)
+				if err != nil {
+					klog.Fatalf("failed to get secret file: %v", err)
+				}
+				// log the secret
+				klog.Infof(secret)
+
+				// watch for errors
+			case err := <-watcher.Errors:
+				klog.Error("failed to watch: %v", err)
+			}
+		}
+	}()
+
+	<-done
+}
+
+// getSecret reads the secret from the pod file system
+func getSecret(secretFile string) (string, error) {
+	// read the secret from pod file system
+	if _, err := os.Stat(secretFile); err != nil {
+		return "", err
+	}
+	bytes, err := os.ReadFile(secretFile)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
