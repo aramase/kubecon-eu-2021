283dd878d610a09853182c59d44f103d854547f2
Anish Ramasekar <anish.ramasekar@gmail.com>
2021-03-30 12:52:04 -0700

    feat: add google secret manager client code
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

---
+++ google-secret-manager/go.mod
@@ -0,0 +1,11 @@
+module github.com/aramase/kubecon-eu-2021/google-secret-manager
+
+go 1.16
+
+require (
+	cloud.google.com/go v0.80.0
+	golang.org/x/oauth2 v0.0.0-20210313182246-cd4f82c27b84
+	google.golang.org/api v0.42.0
+	google.golang.org/genproto v0.0.0-20210323160006-e668133fea6a
+	k8s.io/klog v1.0.0
+)
---
+++ google-secret-manager/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+	"k8s.io/klog"
+)
+
+var (
+	secretName = flag.String("secret-name", "app-secret", "secret name")
+)
+
+func main() {
+	ctx := context.Background()
+
+	// secret name is required
+	if len(*secretName) == 0 {
+		klog.Fatalf("secret name is required")
+	}
+
+	// get the credentials to access secret manager
+	creds, err := google.FindDefaultCredentials(ctx, "https://www.googleapis.com/auth/cloud-platform")
+	if err != nil {
+		klog.Fatalf("failed to get credentials: %v", err)
+	}
+
+	// create the client with the default credentials as token source
+	client, err := secretmanager.NewClient(ctx, []option.ClientOption{option.WithTokenSource(creds.TokenSource)}...)
+	if err != nil {
+		klog.Fatalf("failed to setup client: %+v", err)
+	}
+
+	// Build the request to access the secret
+	accessRequest := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: *secretName,
+	}
+
+	// call the API
+	result, err := client.AccessSecretVersion(ctx, accessRequest)
+	if err != nil {
+		klog.Fatalf("failed to access secret version: %v", err)
+	}
+	// log the secret
+	klog.Info(string(result.Payload.Data))
+}
